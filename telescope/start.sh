d25ea728f5c2ba9060c25d3eca66a20f5b0076e3
freemso <freemso@hotmail.com>
2019-07-30 13:54:11 +0800

    Remove NewRelic.

--- telescope/start.sh
+++ telescope/start.sh
@@ -4,6 +4,4 @@ cd  `dirname $0`
 
 export FLASK_APP=app.py
 
-NEW_RELIC_CONFIG_FILE=newrelic.ini
-export NEW_RELIC_CONFIG_FILE
-newrelic-admin run-program gunicorn --workers 10 --timeout 100 --bind 0.0.0.0:35556 app:app
+gunicorn --workers 10 --timeout 100 --bind 0.0.0.0:35556 app:app
